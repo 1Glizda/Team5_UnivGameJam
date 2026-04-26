3ceb8ff3d645dbb21ed7537ac43a157b5e1985f5
Barsan Patricia <barsanpatricia@gmail.com>
2026-04-26 13:31:56 +0300

    Tutorial popup added

--- Assets/_SANDBOX/Barsi/Animations/DealerAnimator.cs
+++ Assets/_SANDBOX/Barsi/Animations/DealerAnimator.cs
@@ -5,13 +5,19 @@ public class DealerAnimator : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private DialogueComponent dialogue;
 
-    public bool dialogueSpoken = false;
+    private bool dialogueSpoken = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             TogglePopup(true);
+
+            if (!dialogueSpoken)
+            {
+                dialogueSpoken = true;
+                DialogueSystem.Instance.HandleInteraction(dialogue);
+            }
         }
     }
 
@@ -23,17 +29,11 @@ public class DealerAnimator : MonoBehaviour
         }
     }
 
-    public void TogglePopup(bool state)
+    private void TogglePopup(bool state)
     {
         if (animator != null)
         {
-            animator?.SetBool("shouldPopup", state);
-            if(dialogueSpoken == false)
-            {
-                DialogueSystem.Instance.HandleInteraction(dialogue);
-                dialogueSpoken = true;
-            }
-           
+            animator.SetBool("shouldPopup", state);
         }
     }
 }
